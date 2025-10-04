0861dc276abb585fe27c46ecc1f1d96aab79c755
ShetePro <sheteprolin@gmail.com>
2025-10-04 23:24:57 +0800

    feat(Map): add location permission requests and update app.json for background location support

--- components/map/Map.tsx
+++ components/map/Map.tsx
@@ -1,5 +1,24 @@
 import MapView from "react-native-maps";
 import { StyleProp, ViewStyle } from "react-native";
+import * as Location from "expo-location";
+
+async function requestPermissions() {
+  // 前台定位权限
+  let { status: fgStatus } = await Location.requestForegroundPermissionsAsync();
+  if (fgStatus !== "granted") {
+    console.log("前台定位权限被拒绝");
+    return;
+  }
+
+  // 如果你需要后台定位
+  let { status: bgStatus } = await Location.requestBackgroundPermissionsAsync();
+  if (bgStatus !== "granted") {
+    console.log("后台定位权限被拒绝");
+    return;
+  }
+
+  console.log("定位权限全部授予");
+}
 function Map({
   style,
   className,
@@ -7,6 +26,7 @@ function Map({
   style?: StyleProp<ViewStyle>;
   className?: string;
 }) {
+  requestPermissions();
   return (
     <MapView
       className={className}
