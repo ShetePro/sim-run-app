66bf86d198728b2f5bbbcf2d1c99d28af0c83f15
ShetePro <sheteprolin@gmail.com>
2025-10-13 16:19:38 +0800

    feat(run): add simulation functionality for manual run tracking

--- hooks/useRun.ts
+++ hooks/useRun.ts
@@ -7,12 +7,14 @@ import { haversineDistance } from "@/utils/util";
 export function useRun() {
   const [location, setLocation] = useState<any>(null);
   const [distance, setDistance] = useState<number>(1);
+  const [heading, setHeading] = useState<number>(0);
   const [errorMsg, setErrorMsg] = useState<string>("");
   const [isTracking, setIsTracking] = useState(false);
   let calcIndex = 0;
   const [locationSubscription, setLocationSubscription] =
     useState<LocationSubscription | null>(null);
   const [routePoints, setRoutePoints] = useState<any[]>([]);
+  // 计算距离逻辑
   useEffect(() => {
     if (routePoints.length < 2 || routePoints.length <= calcIndex + 1) {
       return;
@@ -21,9 +23,30 @@ export function useRun() {
     const end = routePoints[calcIndex + 1];
     calcIndex++;
     const distance = haversineDistance(start, end);
-    console.log(`计算距离: ${distance} 米`, start, end);
     setDistance((prev) => prev + distance);
   }, [routePoints]);
+
+  // 手动模拟跑步
+  const simulateRun = () => {
+    const point = routePoints.at(-1);
+    let lat = point?.latitude || location.latitude;
+    let lon = point?.longitude || location.longitude;
+    let index = 0;
+    const interval = setInterval(() => {
+      console.log(lat);
+      lat += 0.00003; // 每次增加一点纬度
+      // lon += 0.0001; // 每次增加一点经度
+      const newPoint = {
+        latitude: lat.toFixed(6),
+        longitude: lon,
+        timestamp: Date.now(),
+      };
+      setLocation(newPoint);
+      setRoutePoints((prevPoints) => [...prevPoints, newPoint]);
+      index++;
+    }, 1000);
+  };
+
   // request location permissions
   const requestPermissions = async () => {
     // 请求前台权限
@@ -74,8 +97,11 @@ export function useRun() {
         // 允许等待更长时间来获取高精度位置
         mayShowUserSettingsDialog: false,
       });
-
-      setLocation(locationData.coords);
+      // save heading direction
+      // if (locationData.coords.heading) {
+      //   setHeading(locationData.coords.heading);
+      // }
+      setLocation({ ...locationData.coords });
     } catch (err) {
       setErrorMsg("获取位置信息失败，请检查GPS是否开启。");
     } finally {
@@ -85,32 +111,32 @@ export function useRun() {
   };
   useEffect(() => {
     requestPermissions();
-  });
+  }, []);
   const startTracking = async () => {
     const hasPermission = await requestPermissions();
     if (!hasPermission) return;
 
     setIsTracking(true);
     setRoutePoints([]); // 开始新会话时清空路径
+    simulateRun();
 
-    const subscription = await Location.watchPositionAsync(
-      {
-        accuracy: Location.Accuracy.High, // 高精度模式
-        timeInterval: 1000, // 每 1 秒更新一次
-        distanceInterval: 5, // 每移动 5 米更新一次
-      },
-      (locationUpdate) => {
-        console.log(locationUpdate, "更新位置");
-        const newPoint = {
-          latitude: locationUpdate.coords.latitude,
-          longitude: locationUpdate.coords.longitude,
-          timestamp: locationUpdate.timestamp,
-        };
-        setLocation(newPoint);
-        setRoutePoints((prevPoints) => [...prevPoints, newPoint]);
-      },
-    );
-    setLocationSubscription(subscription);
+    // const subscription = await Location.watchPositionAsync(
+    //   {
+    //     accuracy: Location.Accuracy.High, // 高精度模式
+    //     timeInterval: 1000, // 每 1 秒更新一次
+    //     distanceInterval: 5, // 每移动 5 米更新一次
+    //   },
+    //   (locationUpdate) => {
+    //     const newPoint = {
+    //       latitude: locationUpdate.coords.latitude,
+    //       longitude: locationUpdate.coords.longitude,
+    //       timestamp: locationUpdate.timestamp,
+    //     };
+    //     setLocation(newPoint);
+    //     setRoutePoints((prevPoints) => [...prevPoints, newPoint]);
+    //   },
+    // );
+    // setLocationSubscription(subscription);
   };
   // 3. 停止位置追踪
   const stopTracking = () => {
@@ -124,7 +150,12 @@ export function useRun() {
   };
   // 4. 组件卸载时停止追踪
   useEffect(() => {
+    // const call = setInterval(() => {
+    //   const random = Math.floor(Math.random() * 10) % 10;
+    //   setHeading((prevState) => prevState + random);
+    // }, 1500);
     return () => {
+      // clearInterval(call);
       if (locationSubscription) {
         locationSubscription.remove();
       }
@@ -137,5 +168,6 @@ export function useRun() {
     stopTracking,
     routePoints,
     distance,
+    heading,
   };
 }
